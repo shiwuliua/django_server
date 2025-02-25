62cf08f2b08cff3c96a6345420bb95027ec5fa4a
wuliu <3403456815>
2025-02-25 23:39:22 +0800

    2025/2/25

--- wuliuyun/settings.py
+++ wuliuyun/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5-m0b72zpi8a0df2b416r%)t$bxa_1bha5pg5xqf@r5sf2*t8g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["139.9.199.226"]
 
 
 # Application definition
